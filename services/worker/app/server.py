cbd622ff7b1caad4feafce1cf0a3ca367c2ac162
ctimaru <c.timaru@gmail.com>
2026-09-16 16:33:09 +0200

    feat: mount P1.4 global search API

--- services/worker/app/server.py
+++ services/worker/app/server.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from .bulk_import_api import router as bulk_import_router
 from .data_lifecycle import router as data_lifecycle_router
 from .data_source_center import router as data_source_center_router
+from .global_search import router as global_search_router
 from .main import app
 from .observability import install_observability, router as observability_router
 from .retrieval_api import router as retrieval_router
@@ -10,6 +11,7 @@ from .semantic_index import install_semantic_indexer
 from .tenant_admin import router as tenant_admin_router
 
 app.include_router(retrieval_router)
+app.include_router(global_search_router)
 app.include_router(observability_router)
 app.include_router(data_lifecycle_router)
 app.include_router(tenant_admin_router)
