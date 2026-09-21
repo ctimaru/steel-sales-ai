84b962a57e5ffbb2f8761085bc18d7bc6d788485
ctimaru <c.timaru@gmail.com>
2026-09-21 18:02:10 +0200

    feat: register promotion readiness router

--- services/worker/app/server.py
+++ services/worker/app/server.py
@@ -9,6 +9,7 @@ from .main import app
 from .observability import install_observability, router as observability_router
 from .p1_assistant import router as p1_assistant_router
 from .product_360 import router as product_360_router
+from .promotion_readiness import router as promotion_readiness_router
 from .retrieval_api import router as retrieval_router
 from .semantic_index import install_semantic_indexer
 from .tenant_admin import router as tenant_admin_router
@@ -17,6 +18,7 @@ app.include_router(retrieval_router)
 app.include_router(global_search_router)
 app.include_router(p1_assistant_router)
 app.include_router(product_360_router)
+app.include_router(promotion_readiness_router)
 app.include_router(observability_router)
 app.include_router(data_lifecycle_router)
 app.include_router(tenant_admin_router)
