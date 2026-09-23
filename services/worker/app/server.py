f1c10c43330d2a951fabab60293fd0680700f5a2
ctimaru <c.timaru@gmail.com>
2026-09-23 16:28:15 +0200

    feat: install PA2.30.10 recovery bootstrap

--- services/worker/app/server.py
+++ services/worker/app/server.py
@@ -9,6 +9,7 @@ from .main import app
 from .observability import install_observability, router as observability_router
 from .offer_reparse import install_offer_reparse_bootstrap, router as offer_reparse_router
 from .source_reingest import router as source_reingest_router
+from .source_recovery_bootstrap import install_offer_source_recovery_bootstrap
 from .p1_assistant import router as p1_assistant_router
 from .product_360 import router as product_360_router
 from .promotion_readiness import router as promotion_readiness_router
@@ -32,6 +33,7 @@ app.include_router(evidence_router)
 app.include_router(offer_reparse_router)
 app.include_router(source_reingest_router)
 install_offer_reparse_bootstrap(app)
+install_offer_source_recovery_bootstrap(app)
 install_observability(app)
 install_semantic_indexer(app)
 
